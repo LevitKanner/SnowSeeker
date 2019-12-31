ac994f726192c3f34eef43e0041fb056bb2d25c2
Levit Osei Kanner <lkanner21@gmail.com>
2019-12-31 10:40:37 +0000

    Adds favorite model

--- SnowSeeker/Favorites.swift
+++ SnowSeeker/Favorites.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2019 Levit Kanner. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+class Favorites: ObservableObject {
+    @Published private var resorts: Set<String>
+    
+    private let saveKey = "Favorites"
+    
+    init(){
+        self.resorts = []
+        
+        //Load favorites from userdefaults
+        if let data = UserDefaults.standard.data(forKey: saveKey){
+            if let decodedData = try? JSONDecoder().decode(Set<String>.self, from: data){
+                self.resorts = decodedData
+            }
+        }
+    }
+    
+    var count: Int {
+        resorts.count
+    }
+    
+    func contains(_ resort: Resort) -> Bool{
+        resorts.contains(resort.id)
+    }
+    
+    
+    func add(_ resort: Resort){
+        objectWillChange.send()
+        resorts.insert(resort.id)
+        save()
+    }
+    
+    func remove(_ resort: Resort){
+        objectWillChange.send()
+        resorts.remove(resort.id)
+        save()
+    }
+    
+    func save(){
+        if let encodedData = try? JSONEncoder().encode(resorts){
+            UserDefaults.standard.set(encodedData, forKey: saveKey)
+        }
+    }
+}
